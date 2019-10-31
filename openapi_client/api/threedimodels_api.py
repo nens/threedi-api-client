e95d01f818c321ec59202d6c4ea1988dbd4a2d8c
Lars Claussen <lars.claussen@nelen-schuurmans.nl>
2019-10-31 12:55:39 +0100

    bulk boundaries

--- openapi_client/api/threedimodels_api.py
+++ openapi_client/api/threedimodels_api.py
@@ -681,6 +681,112 @@ class ThreedimodelsApi(object):
             _request_timeout=local_var_params.get('_request_timeout'),
             collection_formats=collection_formats)
 
+    def threedimodels_bulk_boundaryconditions_create(self, threedimodel_pk, data, **kwargs):  # noqa: E501
+        """threedimodels_bulk_boundaryconditions_create  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.threedimodels_bulk_boundaryconditions_create(threedimodel_pk, data, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str threedimodel_pk: (required)
+        :param list[BoundaryCondition] data: (required)
+        :return: list[BoundaryCondition]
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.threedimodels_bulk_boundaryconditions_create_with_http_info(threedimodel_pk, data, **kwargs)  # noqa: E501
+        else:
+            (data) = self.threedimodels_bulk_boundaryconditions_create_with_http_info(threedimodel_pk, data, **kwargs)  # noqa: E501
+            return data
+
+    def threedimodels_bulk_boundaryconditions_create_with_http_info(self, threedimodel_pk, data, **kwargs):  # noqa: E501
+        """threedimodels_bulk_boundaryconditions_create  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.threedimodels_bulk_boundaryconditions_create_with_http_info(threedimodel_pk, data, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str threedimodel_pk: (required)
+        :param list[BoundaryCondition] data: (required)
+        :return: list[BoundaryCondition]
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        local_var_params = locals()
+
+        all_params = ['threedimodel_pk', 'data']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        for key, val in six.iteritems(local_var_params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method threedimodels_bulk_boundaryconditions_create" % key
+                )
+            local_var_params[key] = val
+        del local_var_params['kwargs']
+        # verify the required parameter 'threedimodel_pk' is set
+        if ('threedimodel_pk' not in local_var_params or
+                local_var_params['threedimodel_pk'] is None):
+            raise ValueError("Missing the required parameter `threedimodel_pk` when calling `threedimodels_bulk_boundaryconditions_create`")  # noqa: E501
+        # verify the required parameter 'data' is set
+        if ('data' not in local_var_params or
+                local_var_params['data'] is None):
+            raise ValueError("Missing the required parameter `data` when calling `threedimodels_bulk_boundaryconditions_create`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'threedimodel_pk' in local_var_params:
+            path_params['threedimodel_pk'] = local_var_params['threedimodel_pk']  # noqa: E501
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        if 'data' in local_var_params:
+            body_params = local_var_params['data']
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.select_header_content_type(  # noqa: E501
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['Bearer']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/threedimodels/{threedimodel_pk}/bulk_boundaryconditions/', 'POST',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='list[BoundaryCondition]',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=local_var_params.get('async_req'),
+            _return_http_data_only=local_var_params.get('_return_http_data_only'),  # noqa: E501
+            _preload_content=local_var_params.get('_preload_content', True),
+            _request_timeout=local_var_params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
     def threedimodels_bulk_potentialbreaches_create(self, threedimodel_pk, data, **kwargs):  # noqa: E501
         """threedimodels_bulk_potentialbreaches_create  # noqa: E501
 
