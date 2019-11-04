a06d25428f1e8db95b20f1e9f05be50e9854d99d
larsclaussen <lars.claussen@nelen-schuurmans.nl>
2019-11-04 15:37:47 +0100

    Back to development: 0.0.17.3

--- openapi_client/__init__.py
+++ openapi_client/__init__.py
@@ -15,7 +15,7 @@
 
 from __future__ import absolute_import
 
-__version__ = '0.0.17.2'
+__version__ = '0.0.17.3.dev0'
 
 # import apis into sdk package
 from openapi_client.api.auth_api import AuthApi
