a92fa9e45c90962575ec1873e2f63c1e82f2de69
jpprins1 <jpprins1@gmail.com>
2020-04-01 15:31:40 +0200

    Back to development: 3.0.b16

--- openapi_client/__init__.py
+++ openapi_client/__init__.py
@@ -15,7 +15,7 @@
 
 from __future__ import absolute_import
 
-__version__ = '3.0.b15'
+__version__ = '3.0.b16.dev0'
 
 # import apis into sdk package
 from openapi_client.api.auth_api import AuthApi
