7ff3bd00c0204b65d72e091f50395c49790e79ec
Lars Claussen <lars.claussen@nelen-schuurmans.nl>
2020-04-24 20:59:56 +0200

    Back to development: [3.0.b20

--- openapi_client/__init__.py
+++ openapi_client/__init__.py
@@ -15,7 +15,7 @@
 
 from __future__ import absolute_import
 
-__version__ = '3.0.b19'
+__version__ = '[3.0.b20.dev0'
 
 # import apis into sdk package
 from openapi_client.api.auth_api import AuthApi
