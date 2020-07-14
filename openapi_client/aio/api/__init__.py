a2ea85d06306bb2afbdc870b9a2d6f8d5c955a60
jpprins1 <jpprins1@gmail.com>
2020-07-14 15:14:01 +0200

    removed double models/api

--- openapi_client/aio/api/__init__.py
+++ openapi_client/aio/api/__init__.py
@@ -3,16 +3,4 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from openapi_client.aio.api.auth_api import AuthApi
-from openapi_client.aio.api.contracts_api import ContractsApi
-from openapi_client.aio.api.files_api import FilesApi
-from openapi_client.aio.api.inpy_versions_api import InpyVersionsApi
-from openapi_client.aio.api.organisations_api import OrganisationsApi
-from openapi_client.aio.api.permissions_api import PermissionsApi
-from openapi_client.aio.api.repositories_api import RepositoriesApi
-from openapi_client.aio.api.revisions_api import RevisionsApi
-from openapi_client.aio.api.roles_api import RolesApi
-from openapi_client.aio.api.simulations_api import SimulationsApi
-from openapi_client.aio.api.statuses_api import StatusesApi
-from openapi_client.aio.api.threedimodels_api import ThreedimodelsApi
-from openapi_client.aio.api.usage_api import UsageApi
+from openapi_client.api import *
