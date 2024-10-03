c9604a055051ade85f44c2a7bae283c6af7255e6
daanvaningen <daanvaningen@gmail.com>
2024-10-03 09:07:38 +0200

    fix tests

--- tests/test_threedi_api_client_aio.py
+++ tests/test_threedi_api_client_aio.py
@@ -2,7 +2,8 @@ import pytest
 from threedi_api_client.aio.threedi_api_client import ThreediApiClient
 
 
-def test_init_threedi_api_client_from_env_file(tmpdir):
+@pytest.mark.asyncio
+async def test_init_threedi_api_client_from_env_file(tmpdir):
     env_file = tmpdir / "env_file"
     with open(str(env_file), "w") as f:
         f.write("API_HOST=localhost:8000/v3.0\n")
@@ -12,7 +13,8 @@ def test_init_threedi_api_client_from_env_file(tmpdir):
         ThreediApiClient(env_file=str(env_file))
 
 
-def test_init_threedi_api_client_from_env_vars(monkeypatch):
+@pytest.mark.asyncio
+async def test_init_threedi_api_client_from_env_vars(monkeypatch):
     monkeypatch.setenv("API_HOST", "localhost:8000/v3.0")
     monkeypatch.setenv("API_USERNAME", "username")
     monkeypatch.setenv("API_PASSWORD", "password")
@@ -20,7 +22,8 @@ def test_init_threedi_api_client_from_env_vars(monkeypatch):
         ThreediApiClient()
 
 
-def test_init_threedi_api_client_from_config():
+@pytest.mark.asyncio
+async def test_init_threedi_api_client_from_config():
     config = {
         "API_HOST": "localhost:8000/v3.0",
         "API_USERNAME": "username",
